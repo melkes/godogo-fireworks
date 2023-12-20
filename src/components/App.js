7d8f2c0935d76ffb3481ce143428b1b572bec6b3
Mike Wilkes <mikewilkes@outlook.com>
2023-12-19 21:05:41 -0800

    Changed BrowserRouter to HashRouter for gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import LivingRoomScene from "./LivingRoomScene";
 import Sitting from "./Sitting";
